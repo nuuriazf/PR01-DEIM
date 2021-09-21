34be621d480f1e059480573ef0cca083024c5591
nuuriazf <nuriellez@gmail.com>
2021-09-21 10:13:43 +0200

    Ejercicio 1_B

--- ACTIVIDAD 01 NURIA ZAPATA/Assets/Scripts/Actividades.cs
+++ ACTIVIDAD 01 NURIA ZAPATA/Assets/Scripts/Actividades.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class Actividades : MonoBehaviour
 {
      // ejercicio 1: variables  
-    string nombre = "Nuria";
-    int numLives = 3;
-    float numEnergy = 10;
-    bool alive= true; 
+    [SerializeField] string nombre = "Nuria";
+    [SerializeField] int numLives = 3;
+    public float numEnergy = 10;
+    [SerializeField] bool alive= true; 
     // Start is called before the first frame update
     void Start()
-    {   
+    {
+        print(numEnergy);
+         numEnergy = 20;
+        print(numEnergy);
         
 
         
