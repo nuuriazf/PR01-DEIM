14bde06b832d1f7981d104a8f176c568e3006f83
nuuriazf <nuriellez@gmail.com>
2021-09-21 13:03:16 +0200

    Ejercicio 2_B_for

--- ACTIVIDAD 01 NURIA ZAPATA/Assets/Scripts/Actividad02.cs
+++ ACTIVIDAD 01 NURIA ZAPATA/Assets/Scripts/Actividad02.cs
@@ -4,21 +4,31 @@ using UnityEngine;
 
 public class Actividad02 : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        int m = 0;
+        int n;
+        for (n = 0; n <= 10; n++)
+        {
+           
+            print(n);
+            if (n == 10)
+            {
+                m++;
+                print("Se han completado " + m++ + " ciclos.");
+            }
+
+        }
+
+    
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        int n;
-        for (n=0; n<=10; n++)
-        {
-            print(n); 
-
-        }
-        
+       
     }
 }
